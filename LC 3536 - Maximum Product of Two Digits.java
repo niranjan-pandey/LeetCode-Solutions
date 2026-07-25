a736c5303448efb42a0868de05dc2a03aa418663
niranjan-pandey <niranjanpandey1188@gmail.com>
2026-07-25 16:24:51 +0530

    Add files via upload

---
+++ LC 3536 - Maximum Product of Two Digits.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProduct(int n) {
+        
+        //TC = O(log(n)) base10
+        //SC = O(1)
+        
+        int a = 0;
+        int b = 0;
+        while(n > 0) {
+            int digit = n % 10;
+            if(digit > a) {
+                b = a;
+                a = digit;
+            } else if(digit > b) {
+                b = digit;
+            }
+            n /= 10;
+        }
+        return a * b;
+    }
+}

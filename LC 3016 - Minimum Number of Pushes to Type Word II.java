6b01a9fdb899de565a001eab4174e22d29c7ed16
niranjan-pandey <niranjanpandey1188@gmail.com>
2026-07-31 23:44:34 +0530

    Add files via upload

---
+++ LC 3016 - Minimum Number of Pushes to Type Word II.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumPushes(String word) {
+
+        //TC = O(n)
+        //SC = O(1)
+
+        int[] freq = new int[26];
+        for(char c : word.toCharArray())
+            freq[c-'a']++;    
+
+        Arrays.sort(freq);
+        int result = 0;
+        for(int i = 25, curPos = 0; i >= 0 && freq[i] > 0; --i, ++curPos) 
+            result += (freq[i] * (curPos / 8 + 1));
+
+        return result;
+    }
+}

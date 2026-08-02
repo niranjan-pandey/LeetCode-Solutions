e2df25635c30682eab216d7b361f73b767d1bddb
niranjan-pandey <niranjanpandey1188@gmail.com>
2026-08-02 23:57:38 +0530

    Added another solution

--- LC 3016 - Minimum Number of Pushes to Type Word II.java
+++ LC 3016 - Minimum Number of Pushes to Type Word II.java
@@ -16,3 +16,41 @@ class Solution {
         return result;
     }
 }
+
+//2nd Solutuion
+class Solution {
+    public int minimumPushes(String word) {
+
+        //TC = O(n)
+        //SC = O(26)
+        
+        int[] freq = new int[26];
+        for(char c : word.toCharArray())
+            freq[c-'a']++;
+
+        Arrays.sort(freq);
+        reverse(freq);
+
+        int result = 0;
+        for(int i = 0; i < 26; ++i) {
+            if(freq[i] == 0) 
+                break;
+
+            result += freq[i] * (i / 8 + 1);  
+        }
+        return result;
+    }
+
+    private void reverse(int[] freq) {
+        int i = 0;
+        int j = 25;
+
+        while(i < j) {
+            int temp = freq[i];
+            freq[i] = freq[j];
+            freq[j] = temp;
+            ++i;
+            --j;
+        }
+    }
+}
